208652ef14f90be278cdc4c773910351b71a1515
Anthony Urbina <84246381+magikflowz@users.noreply.github.com>
2021-06-02 14:58:05 -0400

    Update recycle.py

--- recycle.py
+++ recycle.py
@@ -3,19 +3,19 @@ import pyautogui
 
 def recycle():
     try:
-        os.chdir(r"C:\Users\Magik\Desktop")
+        os.chdir(r"C:\Users\Enter user here\Desktop") #enter your user 
         cwd = os.getcwd()
         result = cwd
-        if result == 'C:\\Users\\Magik\\Desktop':
+        if result == 'C:\\Users\\Enter user here\\Desktop': #enter user here
             print("change was successfull!")
     except FileNotFoundError:
         print("Can't find directory\n check your directory input")
         
-    recycle_bin = pyautogui.locateCenterOnScreen(r'C:\Users\Magik\Desktop\python\recycle.png', grayscale=False)
+    recycle_bin = pyautogui.locateCenterOnScreen(r'C:\Users\Enter user here\Desktop\python\recycle.png', grayscale=False)
     pyautogui.rightClick(recycle_bin)
-    empty_bin = pyautogui.locateCenterOnScreen(r'C:\Users\Magik\Desktop\python\empty.png')
+    empty_bin = pyautogui.locateCenterOnScreen(r'C:\Users\Enter user here\Desktop\python\empty.png')
     pyautogui.click(empty_bin)
-    yes = pyautogui.locateCenterOnScreen(r'C:\Users\Magik\Desktop\python\yes.png', grayscale=False)
+    yes = pyautogui.locateCenterOnScreen(r'C:\Users\Enter user here\Desktop\python\yes.png', grayscale=False)
     pyautogui.press(['enter'])
 
 recycle()
